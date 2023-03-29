b66ec2c6291e6930680361fcee350f714650a560
periakteon <masumgokyuz@gmail.com>
2023-03-30 01:59:46 +0300

    onClick özelliği

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import Masum from "./components/Masum.jsx";
 
 function App() {
   const propsDeneme = "props mantığı: ben bir propsum"
+
+  const clickFunction = () => {
+    console.log("butona tıklandı");
+  }
   return (
     <div className="app">
       <Masum propsIcınBoyleYapiyoruz = {propsDeneme}/>
       <Deneme/>
+
+      <button onClick={clickFunction}>tıkla bana</button>
     </div>
   );
 }
