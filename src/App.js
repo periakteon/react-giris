d88a0b6c9ef167b2e12caacea83b4baae1c9e8e6
periakteon <masumgokyuz@gmail.com>
2023-03-29 23:50:12 +0300

    değişkenlerin div içerisinde kullanılması

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 function App() {
+
+  // return'ün üzerinde değişken tanımlanabilir:
+
+  const forHtml = "HTML içinde kullanmak üzere oluşturulmuş bir değişken"
   return (
     <div>
-      asdqwe
+      {/* Bu bir yorum satırıdır */}
+      {/* fonksiyon içerisinde tanımlanmış olan değişkenlere süslü parantezler kullanarak ulaşılabilir. Aşağıdaki gibi: */}
+      {forHtml}
     </div>
   );
 }
