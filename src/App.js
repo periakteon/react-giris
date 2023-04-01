adbab18dcb4e1011541d92e42514c432501db601
periakteon <masumgokyuz@gmail.com>
2023-04-01 18:09:21 +0300

    2) TODO App: Girilen mesajları muhafaza etmek

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import { useState } from 'react';
 function App() {
   // text'i tutmak için state oluşturduk
   const [text, setText] = useState('');
+
+  // (eklemek için) girilen text'i textStore içerisinde muhafaza etmek için array oluşturduk
+  const [textStore, setTextStore] = useState([]);
   const onChangeFunc = (e) => {
     setText(e.target.value);
-  }
+  };
+
+  const addTextStore = () => {
+    // önceden var olan tüm text'leri al, sonra yeni text'i ekle
+    setTextStore(prev => [...prev, text]);
+    // text'i temizle
+    setText('');
+  };
+
+  console.log("textStore:", textStore);
+
   return (
     <>
-      <input value={text} onChange={onChangeFunc} type='text' placeholder='Ekle' />
-      <button>Ekle</button>
+      <input
+        value={text}
+        onChange={onChangeFunc}
+        type='text'
+        placeholder='Ekle'
+      />
+      <button onClick={addTextStore}>Ekle</button>
     </>
   );
 }
