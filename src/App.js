b674bea25c45ad8a991608cd013b63b84aeb2bbb
periakteon <masumgokyuz@gmail.com>
2023-04-01 15:32:58 +0300

    react-slick slider info

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import { useMemo, useState } from 'react';
-
 function App() {
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState("");
-
-  const func = (num) => {
-    console.log('hesaplanıyor.... : ');
-    for (let i = 0; i < 10000; i++) {
-      console.log(i);
-      num++;
-    }
-    return num;
-  };
-
-  // useMemo syntax: useMemo(() => first, [second])
-  // burada aslında şunu demiş oluyoruz: sayfada hangi render işlemi yapılırsa yapılsın (bu örnekte setText değeri değiştikçe sayfa render oluyor) "count" değerinde bir değişiklik olmadığı sürece count'u render etme. Yani yukarıdaki for döngüsü her render işleminde çalışmış olmayacak - count ile ilgili render işlemleri hariç.
-  const memo = useMemo( () => func(count), [count]);
 
   return (
-  <>
-  {memo}
-  <input value={text} onChange={e => setText(e.target.value)} placeholder='ara'/>
-  </>
-  )
+    <>
+    react-slick ile slider oluşturabiliriz. Hazır slider kütüphanesi.
+    </>
+  );
 }
 
 export default App;
